d5529e0e7e26f15c944fe835b4ccbb70076c3627
GolfGrab <surapus.m@ku.th>
2022-02-14 20:33:41 +0700

    done no deliver track

--- components/OrderCard.jsx
+++ components/OrderCard.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 
 function OrderCard({ order }) {
+  const iconSrc = {
+    Success: 'https://www.svgrepo.com/show/395713/accept-mark-check-tick.svg',
+    Canceled:
+      'https://www.svgrepo.com/show/395697/cancel-remove-delete-cross.svg',
+    NowOfficeKorea:
+      'https://www.svgrepo.com/show/395694/estate-building-home-house.svg',
+    NowSentKorea:
+      'https://www.svgrepo.com/show/395725/forecast-storage-weather-cloud.svg',
+    NowThailand:
+      'https://www.svgrepo.com/show/395739/navigation-gps-map-pin-location.svg',
+    NowSentThailand:
+      'https://www.svgrepo.com/show/395705/envelope-letter-email-mail-send.svg',
+    PendingPay:
+      'https://www.svgrepo.com/show/395737/sync-refresh-loading-load.svg',
+    PendingPreOrder:
+      'https://www.svgrepo.com/show/395704/store-shop-shopping-bag.svg',
+    Problem: 'https://www.svgrepo.com/show/395724/focus-aim-goal-target.svg',
+  }
+  const [icon, setIcon] = useState(iconSrc.cancel)
+  useEffect(() => {
+    if (order.orderStatus === 'Success') {
+      setIcon(iconSrc.Success)
+    } else if (order.orderStatus === 'Canceled') {
+      setIcon(iconSrc.Canceled)
+    } else if (order.orderStatus === 'NowOfficeKorea') {
+      setIcon(iconSrc.NowOfficeKorea)
+    } else if (order.orderStatus === 'NowSentKorea') {
+      setIcon(iconSrc.NowSentKorea)
+    } else if (order.orderStatus === 'NowSentThailand') {
+      setIcon(iconSrc.NowSentThailand)
+    } else if (order.orderStatus === 'NowThailand') {
+      setIcon(iconSrc.NowThailand)
+    } else if (order.orderStatus === 'PendingPay') {
+      setIcon(iconSrc.PendingPay)
+    } else if (order.orderStatus === 'PendingPreOrder') {
+      setIcon(iconSrc.PendingPreOrder)
+    } else if (order.orderStatus === 'Problem') {
+      setIcon(iconSrc.Problem)
+    }
+  }, [order])
+
   return (
-    <div className=" m-[1.5rem]   w-[20rem] border  p-[1rem]   px-[1.5rem]">
+    <div className=" m-[1.5rem]   w-[20rem] rounded-xl border-4  border-[#ffc9c2]   bg-[#ffffc2] p-[1rem] px-[1.5rem] transition ease-in hover:scale-105">
       {/* date and icon */}
       <div className="flex  w-full items-center justify-between">
-        <div>{moment(order.createdAt).format(' DD MMM YY , HH:mm')}</div>
+        <div className="opacity-70">
+          {moment(order.createdAt).format(' DD MMM YY , HH:mm')}
+        </div>
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-[3rem] w-[3rem]"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1"
-              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+          <img src={icon} className="h-[2rem] w-[2rem]" />
         </div>
       </div>
       <div className=" text-2xl font-semibold">{order.product.productName}</div>
@@ -29,12 +59,31 @@ function OrderCard({ order }) {
         {order.product.productDescription}
       </div>
       <br />
-      <div className="text-sm">รายละเอียดสินค้า : {order.orderDetails}</div>
-      <br />
+      {order.orderDetails === '' || order.orderDetails === null ? null : (
+        <div>
+          <div className="text-sm">รายละเอียดสินค้า : {order.orderDetails}</div>
+          <br />
+        </div>
+      )}
+
       <div className="text-sm">จำนวนสินค้ารวม : {order.totalAmount}</div>
       <div className="text-sm">ราคารวม : {order.totalPrice} บาท</div>
-      <div className="text-sm">ค้างชำระ : {order.debt} บาท</div>
-      <div className="text-sm">สถานะสินค้า : {order.orderStatus}</div>
+      <div
+        className="text-sm"
+        style={{
+          color: order.debt == 0 ? '#000000' : '#ff4545',
+        }}
+      >
+        ค้างชำระ : {order.debt} บาท
+      </div>
+      <div
+        className="text-sm"
+        style={{
+          color: order.orderStatus === 'Success' ? '#01a66f' : '#f29339',
+        }}
+      >
+        สถานะสินค้า : {order.orderStatus}
+      </div>
 
       {/* <div>{JSON.stringify(order)}</div> */}
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,7 @@ export default function Home() {
       isNaN(phoneNumber) ||
       !/^[0-9]+$/.test(phoneNumber)
     ) {
+      setOrders([])
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -36,13 +37,13 @@ export default function Home() {
           Swal.fire({
             icon: 'question',
             title: 'ไม่พบรายการสั่งซื้อ',
-            text: ' กรุณาตรวจสอบหมายเลขโทรศัพท์ใหม่ด้วยค่ะ',
+            text: ' กรุณาตรวจสอบหมายเลขโทรศัพท์อีกครั้งค่ะ',
             footer: '<a href="#">ติดต่อสอบถามผู้ขาย?</a>',
           })
 
           return
         }
-        Swal.fire('Done!', 'ค้นหารายการสั่งซื้อเสร็จสิ้น', 'success')
+        Swal.fire('Done!', '<br/>ค้นหารายการสั่งซื้อเสร็จสิ้น<br/>', 'success')
         setOrders(res.order)
         setCustomerName(res.customerName)
       })
@@ -52,14 +53,24 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto flex flex-col items-center py-[3rem] ">
+    <div className="container mx-auto flex flex-col items-center  py-[3rem] ">
       <Head>
         <title>Check-Delivery</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {/* header and form */}
       <HeaderAndForm phoneInput={phoneInput} handleSubmit={handleSubmit} />
-      {orders.length !== 0 ? <div>ออเดอร์ของ : {customerName} </div> : null}
+      {orders.length !== 0 ? (
+        <div className="rounded-xl bg-[#d5fafd] p-2 text-lg underline decoration-2 underline-offset-4 ">
+          ออเดอร์ของ : {customerName}
+        </div>
+      ) : null}
       {/* order list */}
       <div className="flex flex-wrap items-center justify-center  ">
         {orders
